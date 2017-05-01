3e29067c143203195ef20ca21d3653bd530ca613
bjtvpatel <ballupatel@gmail.com>
2017-05-01 13:11:25 +0100

    Week-3 day -1
    Baldev_ConsoleApp

---
+++ Week-3/Day-1/Baldev_ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Baldev_ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            decimal secreatNumber = 0;
+
+            decimal guessNumber = 0;
+
+            int counter = 0;
+
+            Random ran = new Random();
+
+            secreatNumber = ran.Next(1,100);
+           
+            do
+            {
+                Console.WriteLine("Type your best guess number");
+                guessNumber = decimal.Parse(Console.ReadLine());
+
+                if (guessNumber == secreatNumber)
+                {
+                    Console.WriteLine("!!Match!!");
+                }
+                else if (guessNumber > secreatNumber)
+                {
+                    Console.WriteLine("Number {0} is greater than: Secreat Number", guessNumber);
+                }
+                else
+                {
+                    Console.WriteLine("Number {0} is less than : Secreat Number ", guessNumber);
+
+                }
+                counter++;
+               
+            } while (counter <= 3);
+
+        }
+    }
+}
