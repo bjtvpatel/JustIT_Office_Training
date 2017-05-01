884769eebe0133ff110865e9572e98e2f64dda01
bjtvpatel <ballupatel@gmail.com>
2017-05-01 13:10:37 +0100

    week-3 day-1
    training

---
+++ Week-3/Day-1/Switch-Exercise/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Exercise
+{
+    class Program
+    {
+
+        //class PresidentialCandidate
+        //{
+
+
+        //}
+            
+            
+        static void Main(string[] args)
+        {
+            string selectCandidate = "";
+            int counter = 0;
+
+            string candidate1 = "George Bush";
+            string candidate2 = "Hilary Clinton";
+            string candidate3= "Barak Obama";
+
+            Console.WriteLine("List of Presidential Candidate: {0} {1} {2} {3} {4} {5}", "\n", candidate1, "\n", candidate2, "\n", candidate3);
+
+            Console.WriteLine("Enter your choice of candidate for vote either in first name, surname or full name");
+
+            while (counter<3) { 
+
+            selectCandidate = Console.ReadLine().ToLower();
+
+            switch(selectCandidate)
+            {
+                //case "George Bush":
+                //case "George":
+                //case "Bush":
+                case "george bush":
+                case "george":
+                case "bush":
+                    Console.WriteLine("You have voted for {0}", candidate1);
+                    break;
+
+                //case "Hilary Clinton":
+                //case "Hilary":
+                //case "Clinton":
+                case "hilary clinton":
+                case "hilary":
+                case "clinton":
+                    Console.WriteLine("You have voted for {1}", candidate2);
+                    break;
+
+                //case "Barak Obama":
+                //case "Barak":
+                //case "Obama":
+                case "barak obama":
+                case "barak":
+                case "obama":
+                    Console.WriteLine("You have voted for {2}", candidate3);
+                    break;
+
+                default:
+                        Console.Write("You entered wrong name. Please try again");
+                        break;
+
+                }
+                                
+                counter++;
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Week-3/Day-1/while-do-loop-Riddle excercise/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_do_loop_Riddle_excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            string riddles1 = "What is brown and sticky?";
+            string riddles2 = "What loses its head in the morning and gets it back at night?";
+
+            string riddles1Ans = "stick";
+            string riddles11Ans = "chocolate";
+
+            string riddles2Ans = "pillow";
+            string riddles22Ans = "bed";
+
+            string ans1 = "";
+            string ans2 = "";
+                    
+            
+            while (counter<4)
+            {
+                Console.WriteLine(riddles1);
+                ans1 = Console.ReadLine();
+                switch (ans1)
+                {
+                    case "stick":
+                    case "chocolate":
+                        Console.WriteLine("You were correct:");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please try again:");
+                        break;
+                }
+
+                counter++;        
+
+            }
+            counter = 0;
+
+            Console.WriteLine(riddles2);
+
+            do
+            {
+                
+                ans2 = Console.ReadLine();
+                switch (ans2)
+                {
+                    case "pillow":
+                    case "bed":
+                        Console.WriteLine("You were correct:");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please try again:");
+                        break;
+                }
+
+                counter++;
+
+            } while (counter < 4);
+
+            Console.ReadLine();
+                       
+
+        }
+    }
+}
