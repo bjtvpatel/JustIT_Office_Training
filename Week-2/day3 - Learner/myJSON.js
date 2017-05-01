3f4f01154888c878a52e2cc9cef915f427a7dbff
bjtvpatel <ballupatel@gmail.com>
2017-05-01 12:41:16 +0100

    Day-3
    training

---
+++ Week-2/day3 - Learner/myJSON.js
@@ -0,0 +1,62 @@
+//This is the Javascript for the JSON training
+//
+//var first = '{"favCar":"BMW", "favTVS":"Lost"}';
+//
+//var myJ = JSON.parse(first);
+//console.log(myJ);
+//
+//var myJ = JSON.stringify(first);
+//console.log(myJ);
+
+//var car = {
+//  
+//  "model":"Audi",
+//  "price": 2500,
+//  "engine": "Petrol"
+//  
+//};
+//
+//console.log(car.engine);
+//
+//var F1drivers =["Lewis", "Kimi", "Alonso"];
+//
+//console.log(F1drivers[2]);
+//
+//var F1 = {
+//  
+//  "data": [ 
+//    
+//    {
+//      
+//      "car": "Mecedes",
+//      "driver": "Lewis",
+//      "wins":2
+//      
+//    }, 
+//    
+//    {
+//      "circuit": "Monaco",
+//      "pointSystem": [
+//        
+//        {
+//          "firstPlace": 25, 
+//          "secondPlace": 18,
+//          "thirdPlace": 15
+//        }
+//        
+//      ],
+//      
+//      "teams":11
+//      
+//    
+//    }
+//      
+//  ]
+//  
+//}
+//
+//console.log(F1.data[1].pointSystem[0].thirdPlace);
+alert("Hello");
+
+
+
