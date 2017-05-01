1a7c53138dbb1423947d5d6684a1c73e3f0d9b8a
bjtvpatel <ballupatel@gmail.com>
2017-05-01 13:06:30 +0100

    Week-3 day-1
    training

---
+++ Week-3/Day-1/Data structure -Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_structure__Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[ , ] numbers = new double[5,5];
+
+            double sum = 0;
+
+            Console.WriteLine("Please enter your numbers until its stops");
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    numbers[i, j] = double.Parse(Console.ReadLine());
+                    sum = sum + numbers[i, j];
+                }
+
+
+            }
+
+            Console.WriteLine("You have entered 25 numbers and I will calculate average soon");
+
+            double average = (sum / numbers.Length);
+
+            Console.WriteLine("Average = " + average);
+            Console.ReadLine();
+        }
+    }
+}
