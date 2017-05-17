87640d00250cc48efb5285395fa1370786c5e183
bjtvpatel <ballupatel@gmail.com>
2017-05-18 00:34:45 +0100

    MVC pro- 2 - trying to deploy but no luck on hosting
    Let see

--- Week-5/TechMVCDatabase/TechMVCDatabase/Controllers/HomeController.cs
+++ Week-5/TechMVCDatabase/TechMVCDatabase/Controllers/HomeController.cs
@@ -84,14 +84,14 @@ namespace TechMVCDatabase.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Tech-Gadget Record Management System.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact me.";
 
             return View();
         }
